11089a2b320b30b86db357fdc393511268c0e37a
Kartik Nillawar <kartiknillawar@gmail.com>
2022-01-20 14:04:43 +0530

    added cors

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 connectDB();
 
-
+//cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+app.use(cors(corsOptions))
 app.use(express.json());
 
 //templates engine
