1dab2482f4beeacc8f6c702a2bad088824ba71ce
Kartik Nillawar <kartiknillawar@gmail.com>
2022-05-27 21:53:52 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const File  = require('./models/file')
+const fs = require('fs')
+const connectDB = require('./config/db')
+connectDB();
+
+async function fetchData(){
+    const pastDate = new Date(Date.now()- 24*60*60*1000);
+
+    //fetch all files which are older than 24 hr's
+    const files = await File.find({createdAt:{$lt: pastDate}})
+
+    if(files.length){
+        for(const file of files){
+            try{
+                fs.unlinkSync(file.path);
+
+            await file.remove();
+
+            console.log(`succesfully deleted ${file.filename}`);
+            }
+            catch(err){
+                console.log(`error  while deleting file ${err}`);
+            }
+        }
+        console.log(`job done`);
+    }
+}
+
+fetchData().then(process.exit);
